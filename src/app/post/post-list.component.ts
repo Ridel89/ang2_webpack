6cd5a5b40ae665da67deeb530f19e77fd623528a
Victor Kravchuk <V.Kravchuk89@outlook.com>
2017-03-30 13:18:29 +0300

    first commit

---
+++ src/app/post/post-list.component.ts
@@ -0,0 +1,28 @@
+﻿import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
+import { HTTPTestService } from '../services/test.service';
+
+@Component({
+    selector: 'post-list',
+    templateUrl: 'post-list.component.html',
+    styleUrls: ['post-list.component.css']
+})
+
+export class PostListComponent {
+    private posts: any[] = [];
+
+    constructor(private tstService: HTTPTestService,
+                private router: Router) {}
+
+    public ngOnInit() {
+        this.getSurvList();
+    }
+
+    public getSurvList() {
+        this.tstService.getPosts()
+            .subscribe((posts) => {
+                this.posts = posts;
+            });
+    }
+
+}
